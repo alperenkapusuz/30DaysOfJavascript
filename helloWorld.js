c9fdff72ec9058bfb78fb35a663506f2f852478b
Alperen Kapusuz <sefalperen21@gmail.com>
2022-12-09 22:11:29 +0300

    first day

---
+++ helloWorld.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+let firstName;
+console.log(firstName);
+
+let emptyValue = null;
+console.log(null);
+
+console.log(typeof "Asabeneh");
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null); // null objedir
+console.log(typeof undefined);
+
+// degiskenler hafızada yer tutar
